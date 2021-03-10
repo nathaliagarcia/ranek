e1ab6248e4f38f199284d1c4e13e80e8d04ca12b
Nathalia Garcia <nathalia.gm1@gmail.com>
2021-03-10 11:56:07 -0300

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">Ranek</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
