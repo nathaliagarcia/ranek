00879c64310631ec527e2f668e714b52f3904443
Nathalia Garcia <nathalia.gm1@gmail.com>
2021-03-10 18:38:04 -0300

    create header structure

---
+++ components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <nav class="container">
+      <n-link to="/" class="logo">
+        <img src="@/assets/svg/ranek.svg" alt="Logo Ranek"
+      /></n-link>
+      <n-link to="/login" class="btn">Vender / Login</n-link>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
